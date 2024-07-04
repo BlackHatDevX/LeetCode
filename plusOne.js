0343be8920d7748776fee08d149ad8ba5b9fcd48
BlackHatDevX <jashgro@yandex.com>
2024-07-05 00:05:39 +0530

    plusOne

--- plusOne.js
+++ plusOne.js
@@ -1,18 +1,24 @@
-// https://leetcode.com/problems/plus-one/submissions/1309700225/
+// https://leetcode.com/problems/plus-one/solutions/5417478/plus-one-javascript-99-49-beats/
 
 const digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
 
 var plusOne = function (digits) {
-  let d = "";
+  // defining empty string to push values from array
+  let digitsToString = "";
   digits.map((e) => {
-    d = d + e;
+    digitsToString = digitsToString + e;
   });
-  let newd = BigInt(d) + 1n;
-  newd = String(newd);
+  // converting digits to BigInt and adding 1 to it
+  let digitsToInt = BigInt(digitsToString) + 1n;
+
+  // converting digits + 1 to string so we can use it as array
+  AnsToString = String(digitsToInt);
+
   let finalArray = [];
-  for (var a = 0; a < newd.length; a++) {
-    let k = newd[a];
-    finalArray.push(Number(k));
+  // pushing every value(after converting into number) of string to array
+  for (var a = 0; a < AnsToString.length; a++) {
+    let temp = AnsToString[a];
+    finalArray.push(Number(temp));
   }
   return finalArray;
 };
