ebecbd62b16ed5508c3e3fdcbcd4343e19372b17
BlackHatDevX <jashgro@yandex.com>
2024-07-30 18:02:32 +0530

    Linked List

---
+++ LinkedList/BinLinkedListToInt.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function (head) {
+  var num = "";
+  while (head != null) {
+    num = num + head.val;
+    head = head.next;
+  }
+  var ans = parseInt(num, 2);
+  return ans;
+};
---
+++ LinkedList/middleOfLinkedList.js
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let nums = [];
+  let dHead = head;
+  while (dHead) {
+    nums.push(dHead.val);
+    dHead = dHead.next;
+  }
+  let mid = 0;
+  if (nums.length % 2 == 0) {
+    mid = nums.length / 2 + 1;
+  } else {
+    mid = (nums.length + 1) / 2;
+  }
+  console.log(mid);
+  let count = 0;
+  while (head) {
+    if (count != mid - 1) {
+      count = count + 1;
+      head = head.next;
+    } else {
+      return head;
+    }
+  }
+};
